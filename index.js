3a3a906cd8e8c661a2842ae93581eb926124109a
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-14 11:39:35 -0500

    enroll in summer school

--- index.js
+++ index.js
@@ -65,10 +65,22 @@ function personMaker() {
 
   //Student Report
   function makeStudentsReport(data) {
-    // your code here
     let report = [];
     for(let i=0; i<data.length; i++){
       report.push(`${data[i].name}: ${data[i].grade}`);
     }
     return report;
+  }
+
+  //Enroll in Summer School
+  function enrollInSummerSchool(students) {
+    let enrolled = [];
+    for (let i = 0; i < students.length; i++) {
+      enrolled.push({
+        name: students[i].name,
+        status: 'In Summer school',
+        course: students[i].course
+      });
+    }
+    return enrolled;
   }
