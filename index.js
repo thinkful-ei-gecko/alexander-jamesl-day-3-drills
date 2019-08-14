c71b778bfe9c6abe0c55a3463660cedb5617f69d
James Lee <jameswlee109@gmail.com>
2019-08-14 11:32:41 -0500

    student report drill

--- index.js
+++ index.js
@@ -35,3 +35,13 @@ function personMaker() {
     }
     return person;
   }
+
+  //Student Report
+  function makeStudentsReport(data) {
+    // your code here
+    let report = [];
+    for(let i=0; i<data.length; i++){
+      report.push(`${data[i].name}: ${data[i].grade}`);
+    }
+    return report;
+  }
