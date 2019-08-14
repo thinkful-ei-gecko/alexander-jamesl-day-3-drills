47b4a074f3df839865505177b4c47806329307ae
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-14 11:01:34 -0500

    object updater solution

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function createMyObject() {
     }
     }
   }
+
+//Object Updater
+function updateObject(obj) {
+  // your code here
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+  
+  return obj;
+}
