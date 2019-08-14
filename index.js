e945ded4067a02fb6bb82d5bee55b46cd76d215c
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-14 11:08:48 -0500

    line up

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+'use strict';
+
 //Object Creator
 function createMyObject() {
-    // your code here
-    return{
+  return{
     foo: 'bar',
     answerToUniverse: 42,
     'olly olly': 'oxen free',
     sayHello: function(){
       return 'hello'
     }
-    }
   }
+}
 
 //Object Updater
 function updateObject(obj) {
-  // your code here
-    obj.foo = 'foo';
-    obj.bar = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
+
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
   
   return obj;
 }
